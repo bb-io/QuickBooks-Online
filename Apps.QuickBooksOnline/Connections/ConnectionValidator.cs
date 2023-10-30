ee0c8da155b8dd402cc144902a527675c5790598
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-10-30 18:04:45 +0200

    Artifactory

---
+++ Apps.QuickBooksOnline/Connections/ConnectionValidator.cs
@@ -0,0 +1,15 @@
+﻿
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+
+namespace Apps.QuickBooksOnline.Connections
+{
+    public class ConnectionValidator : IConnectionValidator
+    {
+        public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+       IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+        {
+            return new() { IsValid = true };
+        }
+    }
+}
