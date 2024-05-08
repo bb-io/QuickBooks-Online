4cc054b63f66bc66813bf1e93ea445aedf512fd3
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-05-08 11:01:16 +0300

    Logging

--- Apps.QuickBooksOnline/Webhooks/BridgeService.cs
+++ Apps.QuickBooksOnline/Webhooks/BridgeService.cs
@@ -11,17 +11,27 @@ public class BridgeService(string bridgeServiceUrl)
     
     public async Task Subscribe(string url, string id, string subscriptionEvent)
     {
+        var logger = new Logger();
+        
         try
         {
             var bridgeSubscriptionRequest =
                 CreateBridgeRequest($"/webhooks/{AppName}/{id}/{subscriptionEvent}", Method.Post);
             bridgeSubscriptionRequest.AddBody(url);
+            
+            await logger.LogAsync(new
+            {
+                BridgeServiceUrl = bridgeServiceUrl,
+                AppName = AppName,
+                Id = id,
+                SubscriptionEvent = subscriptionEvent,
+                Url = url
+            });
 
             await _bridgeClient.ExecuteAsync(bridgeSubscriptionRequest);
         }
         catch (Exception e)
         {
-            var logger = new Logger();
             await logger.LogAsync(new
             {
                 BridgeServiceUrl = bridgeServiceUrl,
