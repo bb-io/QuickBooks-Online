e911804da718ff4418fcfd6026443f5f1b2d0aab
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-05-13 10:07:58 +0300

    Added data source handlers to create invoice action

--- Apps.QuickBooksOnline/Models/Requests/Invoices/CreateInvoiceRequest.cs
+++ Apps.QuickBooksOnline/Models/Requests/Invoices/CreateInvoiceRequest.cs
@@ -1,13 +1,15 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.QuickBooksOnline.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.QuickBooksOnline.Models.Requests.Invoices;
 
 public class CreateInvoiceRequest
 {
-    [Display("Customer ID")]
+    [Display("Customer ID"), DataSource(typeof(CustomerDataSource))]
     public string CustomerId { get; set; }
 
-    [Display("Item IDs")]
+    [Display("Item IDs"), DataSource(typeof(ItemDataSourceHandler))]
     public IEnumerable<string> ItemIds { get; set; }
     
     [Display("Line amounts")]
