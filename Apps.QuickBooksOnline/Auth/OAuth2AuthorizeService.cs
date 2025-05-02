4e402d1dcb40bd7e2ef57a199b9be38b91d05ea8
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-05-02 16:16:50 +0300

    feat(auth): 🚀 Update redirect URIs for OAuth2 authorization flow; Changed `redirect_uri` to use `BridgeServiceUrl` for consistency; Added `authorization_url` parameter to enhance request details

--- Apps.QuickBooksOnline/Auth/OAuth2AuthorizeService.cs
+++ Apps.QuickBooksOnline/Auth/OAuth2AuthorizeService.cs
@@ -9,6 +9,7 @@ public class OAuth2AuthorizeService(InvocationContext invocationContext)
 {
     public string GetAuthorizationUrl(Dictionary<string, string> values)
     {
+        var bridgeOauthUrl = $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/oauth";
         var endpoints = GetOAuthEndpoints();
         
         var credentials = ApplicationConstants.GetCreds(values);
@@ -17,10 +18,12 @@ public class OAuth2AuthorizeService(InvocationContext invocationContext)
             { "client_id", credentials.ClientId },
             { "response_type", "code" },
             { "scope", credentials.Scope },
-            { "redirect_uri", InvocationContext.UriInfo.AuthorizationCodeRedirectUri.ToString() },
+            { "redirect_uri", $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}/AuthorizationCode" },
+            { "authorization_url", endpoints.AuthorizationEndpoint },
+            { "actual_redirect_uri", InvocationContext.UriInfo.AuthorizationCodeRedirectUri.ToString() },
             { "state", values["state"] }
         };
         
-        return QueryHelpers.AddQueryString(endpoints.AuthorizationEndpoint, parameters);
+        return QueryHelpers.AddQueryString(bridgeOauthUrl, parameters);
     }
 }
